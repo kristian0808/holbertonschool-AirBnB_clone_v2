bb54be24fa9943bb2e509f36054b32ef39b6cd37
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-25 11:23:53 +0100

    fix pycodestyle

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] =='}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,20 +118,20 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        
+
         args_list = args.split()
         class_name = args_list[0]
         if class_name not in HBNBCommand.classes:
             print("** class does not exist **")
             return
-        
+
         new_instance = HBNBCommand.classes[class_name]()
-        
+
         for arg in args_list[1:]:
             parameters = arg.split('=')
             key = parameters[0]
             value = parameters[1]
-            
+
             if value[0] == "\"":
                 value = value.replace("\"", "").replace("_", " ")
             elif "." in value:
@@ -205,7 +205,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -338,5 +338,7 @@ class HBNBCommand(cmd.Cmd):
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
+
 if __name__ == "__main__":
+
     HBNBCommand().cmdloop()

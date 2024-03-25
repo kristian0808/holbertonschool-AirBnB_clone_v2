83c6dde68e171d09ea6e054361ab385220f3f86c
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-25 10:38:55 +0100

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges on 'hbnb_dev_db' to 'hbnb_dev' user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on 'performance_schema' to 'hbnb_dev' user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

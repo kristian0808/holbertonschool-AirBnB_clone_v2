915e3e4c602f86256994eeaf8ab6e5141505d33a
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-26 09:58:12 +0100

    DB storage user

--- models/user.py
+++ models/user.py
@@ -1,23 +1,14 @@
-#!/usr/bin/python3
-"""This is the user class"""
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
+Base = declarative_base()
+
+class BaseModel:
+    pass
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)

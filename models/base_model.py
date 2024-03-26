4c330e5db7e6620585ddadf2c730d92ae833533e
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-26 13:37:36 +0100

    update code

--- models/base_model.py
+++ models/base_model.py
@@ -10,39 +10,26 @@ from sqlalchemy import Column, Integer, String, DateTime
 Base = declarative_base()
 
 
+Base = declarative_base()
+
+
 class BaseModel:
-    """This class will defines all common attributes/methods
-    for other classes
-    """
-    id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instantiation of base model class
-        Args:
-            args: it won't be used
-            kwargs: arguments for the constructor of the BaseModel
-        Attributes:
-            id: unique id generated
-            created_at: creation date
-            updated_at: updated date
-        """
         if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-            if "id" not in kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    setattr(self, k, v)
+            if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                self.created_at = datetime.now()
-            if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.utcnow().strftime(time_format)
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.utcnow().strftime(time_format)
 
     def __str__(self):
         """returns a string
@@ -63,20 +50,10 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
-        """creates dictionary of the class  and returns
-        Return:
-            returns a dictionary of all the key values in __dict__
-        """
-        my_dict = dict(self.__dict__)
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        if '_sa_instance_state' in my_dict.keys():
-            del my_dict['_sa_instance_state']
-        return my_dict
-
     def delete(self):
-        """ delete object
-        """
         models.storage.delete(self)
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy.pop('_sa_instance_state', None)
+        return dict_copy
--- models/city.py
+++ models/city.py
@@ -1,17 +1,10 @@
-#!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
+# models/city.py
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-from models.place import Place
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel, Base):
-    """This is the class for City"""
-    __tablename__ = "cities"
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")

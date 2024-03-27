979cf031f44e9a3382e2ce59908cc1d0e129c450
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-27 09:36:42 +0100

    updated code

--- console.py
+++ console.py
@@ -334,7 +334,7 @@ class HBNBCommand(cmd.Cmd):
                 if att_name in HBNBCommand.types:
                     att_val = HBNBCommand.types[att_name](att_val)
 
-                # update dictionary with name, value pair
+                # update dictionary with name and value pair
                 new_dict.__dict__.update({att_name: att_val})
 
         new_dict.save()  # save updates to file

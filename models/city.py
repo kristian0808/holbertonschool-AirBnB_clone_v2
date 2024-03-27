d81fd7bbf6fa63b61c68d044a0df0e645326a562
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-27 09:19:44 +0100

    city updated code

--- models/city.py
+++ models/city.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from models.place import Place
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
+    """The city class, contains state ID and name."""
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities")
-
-    def __init__(self, *args, **kwargs):
-        """
-            Init for inherited
-        """
-        super().__init__(*args, **kwargs)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")

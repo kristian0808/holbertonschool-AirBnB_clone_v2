05b225b0b09ae9141c26273731353e896daff4b9
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-28 22:07:42 +0100

    add code

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,40 +1,53 @@
-#!/usr/bin/python3
-""" """
+import unittest
 from unittest.mock import patch
-from io import StringIO
-
-from tests.test_models.test_base_model import test_basemodel
 from console import HBNBCommand
 
+class TestHBNBCommand(unittest.TestCase):
+    def setUp(self):
+        self.hbnb_cmd = HBNBCommand()
 
-class test_console(test_basemodel):
-    """ """
+    def test_prompt(self):
+        self.assertEqual(self.hbnb_cmd.prompt, '(hbnb) ')
 
-    def test_class_doc(self):
-        """ """
-        doc = HBNBCommand.__doc__
-        self.assertTrue(doc)
+    def test_precmd(self):
+        line = 'create BaseModel'
+        new_line = self.hbnb_cmd.precmd(line)
+        self.assertEqual(new_line, 'create BaseModel')
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_show_no_name(self, mock_stdout):
-        command = ''
-        HBNBCommand().do_show(command)
-        self.assertEqual(mock_stdout.getvalue(), "** class name missing **\n")
+    def test_do_quit(self):
+        with self.assertRaises(SystemExit):
+            self.hbnb_cmd.do_quit(None)
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_show_no_class(self, mock_stdout):
-        command = 'Fake'
-        HBNBCommand().do_show(command)
-        self.assertEqual(mock_stdout.getvalue(), "** class doesn't exist **\n")
+    @patch('builtins.print')
+    def test_help_quit(self, mock_print):
+        self.hbnb_cmd.help_quit()
+        mock_print.assert_called_with("Exits the program with formatting\n")
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_show_no_id(self, mock_stdout):
-        command = 'City'
-        HBNBCommand().do_show(command)
-        self.assertEqual(mock_stdout.getvalue(), "** instance id missing **\n")
+    def test_do_EOF(self):
+        with patch('sys.exit') as mock_exit:
+            self.hbnb_cmd.do_EOF(None)
+            mock_exit.assert_called_once()
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_show_wrong_id(self, mock_stdout):
-        command = 'City 123123'
-        HBNBCommand().do_show(command)
-        self.assertEqual(mock_stdout.getvalue(), "** no instance found **\n")
+    @patch('builtins.print')
+    def test_help_EOF(self, mock_print):
+        self.hbnb_cmd.help_EOF()
+        mock_print.assert_called_with("Exits the program without formatting\n")
+
+    def test_emptyline(self):
+        self.assertIsNone(self.hbnb_cmd.emptyline())
+
+    def test_key_value_parser(self):
+        args = ['name="John"', 'age=25', 'is_active=True']
+        result = self.hbnb_cmd._key_value_parser(args)
+        expected_result = {'name': 'John', 'age': 25, 'is_active': True}
+        self.assertEqual(result, expected_result)
+
+    @patch('builtins.print')
+    def test_do_create(self, mock_print):
+        with patch.object(HBNBCommand.classes['BaseModel'], 'save') as mock_save:
+            self.hbnb_cmd.do_create('BaseModel name="test" age=20 is_active=True')
+            mock_save.assert_called_once()
+            mock_print.assert_called_with(mock_save().id)
+
+if __name__ == '__main__':
+    unittest.main()

ca55435262ea5d1a0021be1643db2e03eece4b70
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-28 09:53:17 +0100

    update console code

--- console.py
+++ console.py
@@ -2,16 +2,14 @@
 """ Console Module """
 import cmd
 import sys
-import models
+import shlex
 from models.base_model import BaseModel
-from models.__init__ import storage
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-import shlex
 
 
 class HBNBCommand(cmd.Cmd):
